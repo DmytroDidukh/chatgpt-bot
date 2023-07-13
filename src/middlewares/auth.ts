64040cd3fad26e770b7197a89cb18a973ddebfb7
Dmytro Didukh <jeriko.outback@gmail.com>
2023-07-13 21:34:32 +0300

    auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import config from 'config/config';
+import { IContext } from 'typescript/interfaces';
+
+const authorizedUsers: string[] = config.AUTHORIZED_USERS.split(',');
+
+function isAuthenticated(ctx: IContext, next: () => Promise<void>) {
+  if (ctx.session?.authenticated) {
+    return next()
+  }
+
+  const userId = ctx.message.from.id.toString();
+  const isUserAuthorized = authorizedUsers.includes(userId);
+
+  if (isUserAuthorized) {
+    ctx.session ??= { authenticated: true };
+    return next()
+  }
+
+  return ctx.reply('ERROR: FORBIDDEN')
+}
+
+const authMiddleware = { isAuthenticated };
+
+export { authMiddleware };
