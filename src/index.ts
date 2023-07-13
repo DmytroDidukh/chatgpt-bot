eadbb06305d18239b4e1a1484939406124219dfb
Dmytro Didukh <jeriko.outback@gmail.com>
2023-07-13 22:02:22 +0300

    handlers usage

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-import { Context, Telegraf, session } from "telegraf";
+import { Telegraf, session } from "telegraf";
 import { message } from "telegraf/filters";
 import config from 'config/config';
 import { authMiddleware } from 'middlewares/auth';
+import { startHandler, textHandler } from 'handlers/index';
 import { IContext } from 'typescript/interfaces';
 
 const bot = new Telegraf<IContext>(config.BOT_TOKEN);
 
+// MIDDLEWARES
 bot.use(session())
 bot.use(authMiddleware.isAuthenticated)
 
-bot.on(message("text"), async (ctx: Context) => {
-  await ctx.reply("Hello World!");
-});
-
-bot.command("start", async (ctx: Context) => {
-  await ctx.reply("Let's chat!");
-});
+// HANDLERS
+bot.on(message("text"), textHandler);
+bot.command("start", startHandler);
 
 // TODO: add error handling
-function startBot() {
+function launchBot() {
   console.log("BOT STARTED");
 
   bot.launch();
 }
 
-startBot()
+launchBot()
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"))
