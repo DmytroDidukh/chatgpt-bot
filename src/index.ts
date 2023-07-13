24a9939cb9381e6c91c2c87a57f3b29e6e6b66a6
Dmytro Didukh <jeriko.outback@gmail.com>
2023-07-13 21:34:49 +0300

    bot init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+import { Context, Telegraf, session } from "telegraf";
+import { message } from "telegraf/filters";
+import config from 'config/config';
+import { authMiddleware } from 'middlewares/auth';
+import { IContext } from 'typescript/interfaces';
+
+const bot = new Telegraf<IContext>(config.BOT_TOKEN);
+
+bot.use(session())
+bot.use(authMiddleware.isAuthenticated)
+
+bot.on(message("text"), async (ctx: Context) => {
+  await ctx.reply("Hello World!");
+});
+
+bot.command("start", async (ctx: Context) => {
+  await ctx.reply("STARTED!");
+});
+
+bot.launch().then(() => console.log("BOT STARTED"));
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"))
