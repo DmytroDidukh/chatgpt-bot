c0f7569134f2b0563a9f527dc66aff9251bc1dc0
Dmytro Didukh <jeriko.outback@gmail.com>
2023-07-31 13:38:26 +0300

    Add GPT-3.5-turbo model

--- src/consts/openai.ts
+++ src/consts/openai.ts
@@ -1,5 +1,6 @@
 enum OPENAI_MODELS {
     DAVINCI = 'text-davinci-003',
+    GPT_3_5_TURBO = 'gpt-3.5-turbo',
 }
 
 export { OPENAI_MODELS };
--- src/services/openai.ts
+++ src/services/openai.ts
@@ -10,9 +10,9 @@ const openai = new OpenAIApi(configuration);
 
 async function getAnswer(question: string): Promise<string> {
     try {
-        const response = await openai.createCompletion({
-            model: OPENAI_MODELS.DAVINCI,
-            prompt: question,
+        const response = await openai.createChatCompletion({
+            model: OPENAI_MODELS.GPT_3_5_TURBO,
+            messages: [{ content: question, role: 'user' }],
             temperature: 0.5,
             max_tokens: 1000,
             top_p: 1.0,
@@ -20,7 +20,7 @@ async function getAnswer(question: string): Promise<string> {
             presence_penalty: 0.0,
         });
 
-        return response.data.choices[0].text;
+        return response.data.choices[0].message.content;
     } catch (e) {
         console.log('OPENAI ERROR: ', e);
     }
